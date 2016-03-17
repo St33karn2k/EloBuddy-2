807b68a1d9b78acb684a84ef7b2e2c4ab3862fda
zzsnoozezzEB <zz_snooze_zz@hotmail.com>
2016-03-17 18:56:55 +1000

    CameraFocus

---
+++ CameraFocus/CameraFocus/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+
+namespace CameraFocus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += GameLoaded;
+        }
+
+        private static void GameLoaded(EventArgs args)
+        {
+            Game.OnTick += (EventArgs Tick) =>
+            Camera.ScreenPosition = Player.Instance.Position.To2D();
+        }
+    }
+}
